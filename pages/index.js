a90798a0c294c42dc89973a350b44997dd182a17
oguzhan.cevik <oguzhan@90pixel.com>
2023-03-25 19:01:30 +0300

    fix mood color and image part

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ import { useState, useEffect } from 'react'
 export default function Home() {
   const [mood, setMood] = useState(5)
   const [image, setImage] = useState('/neutral.gif')
+  const [moodText, setMoodText] = useState('Neutral')
 
   useEffect(() => {
     setImage(changeImage(mood))
@@ -14,22 +15,27 @@ export default function Home() {
   const changeImage = mood => {
     if (mood < 3) {
       //depressed
+      setMoodText('Depressed')
       return '/depressed.gif'
     } else if (mood < 5) {
       //bored
+      setMoodText('Bored')
       return '/bored.gif'
     } else if (mood == 5) {
-      console.log('5')
       //neutral
+      setMoodText('Neutral')
       return '/neutral.gif'
     } else if (6 <= mood && mood < 8) {
       //happy
+      setMoodText('Happy')
       return '/happy.gif'
     } else if (8 <= mood) {
       //excited
+      setMoodText('Excited')
       return '/excited.gif'
     }
   }
+
   return (
     <>
       <Head>
@@ -46,14 +52,41 @@ export default function Home() {
           <p>Movie suggestion app according to your mood.</p>
           <Content>
             <ImageContainer src={image} alt="Emoji" width={250} height={300} />
-            <RangeInput min={0} max={10} onChange={e => setMood(e.target.value)} type="range" />
-            {mood}
+            <RangeInput
+              mood={mood}
+              min={0}
+              max={10}
+              onChange={e => setMood(e.target.value)}
+              type="range"
+            />
+
+            <MoodText mood={mood}>{moodText}</MoodText>
           </Content>
         </Container>
       </MainContainer>
     </>
   )
 }
+
+const colorPicker = mood => {
+  return mood < 3
+    ? '#00d0fd'
+    : mood < 5
+    ? '#ff8c00'
+    : mood == 5
+    ? '#000000'
+    : mood < 8
+    ? '#ff11ef'
+    : '#ff0a0a'
+}
+const MoodText = styled.h3`
+  width: 200px;
+  padding: 1rem 2rem;
+  color: ${props => colorPicker(props.mood)};
+  transition: color 1s;
+  font-size: 1.5rem;
+  font-weight: 400;
+`
 const RangeInput = styled.input`
   &::-webkit-slider-thumb {
     -webkit-appearance: none;
@@ -83,7 +116,8 @@ const RangeInput = styled.input`
   width: 40%;
   height: 20px;
   border-radius: 5px;
-  background: #d3d3d3;
+  background: ${props => colorPicker(props.mood)};
+  transition: background-color 1s;
 `
 const ImageContainer = styled(Image)`
   object-fit: contain;
